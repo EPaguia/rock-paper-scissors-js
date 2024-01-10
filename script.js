55627367f7c0f2f1ebaf80ef012e4c1224d706c2
EPaguia <144369394+EPaguia@users.noreply.github.com>
2024-01-10 11:42:54 +0800

    fixed html buttons

--- script.js
+++ script.js
@@ -4,25 +4,9 @@ function getComputerChoice(){
     return computerChoice;
 }
 
-function play(playerChoice = prompt('Rock, Paper or Scissors?'), computerChoice = getComputerChoice()){
-    if (playerChoice == null) {
-        return ('No input')
-    }
-    else {let playerSelection = playerChoice.toLowerCase();
-    if ((playerSelection == 'rock' && computerChoice == 'rock') || (playerSelection =='paper' && computerChoice =='paper') || (playerSelection =='scissors' && computerChoice =='scissors')){
-        return [playerSelection, computerChoice, "It's a tie!"]
-    }
-    else if ((playerSelection == 'rock' && computerChoice =='scissors') || (playerSelection == 'paper' && computerChoice =='rock') || (playerSelection == 'scissors' && computerChoice == 'paper')) {
-        return [playerSelection, computerChoice, "You Win!"]
-    }
-    else if ((playerSelection == 'rock' && computerChoice == 'paper') || (playerSelection =='scissors' && computerChoice == 'rock') || (playerSelection == 'paper' && computerChoice == 'scissors')) {
-        return [playerSelection, computerChoice , 'You lose!']
-    }
-    else if (playerSelection != 'rock'|| 'paper' || 'scissors') {
-        return ("Input not recognized.")
-    }
-}
-}
+const rock = document.querySelector('.rock');
 
-
+rock.addEventListener("click", e => {
+    console.log('pwet')
+})
 
