e432f421a878264bbc9d64626209aee85f9a0a22
EPaguia <144369394+EPaguia@users.noreply.github.com>
2024-01-11 22:46:24 +0800

    Added playround function

--- script.js
+++ script.js
@@ -5,8 +5,30 @@ function getComputerChoice(){
 }
 
 const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const result = document.querySelector('.result');
+const player = document.querySelector('.player');
+const computer = document.querySelector('.computer');
 
-rock.addEventListener("click", e => {
-    console.log('pwet')
-})
+
+
+function playround() {
+    let playerSelection = prompt('Rock, Paper, or Scissors');
+    let computerSelection = getComputerChoice();
+    let win = 'You win!';
+    let lose = 'You lose!';
+    let tie = 'It\'s a tie';
+    playerSelection = playerSelection.toLowerCase();
+    if ((playerSelection == 'rock' && computerSelection == 'rock') || (playerSelection == 'paper' && computerSelection == 'paper') || (playerSelection == 'scissors' && computerSelection == 'scissors'))
+        {return tie}
+    else if ((playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'rock')){
+        return lose}
+    else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')){
+        return win}
+    
+}
+
+
+
 
