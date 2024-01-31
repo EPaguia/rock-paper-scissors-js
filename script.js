227a3bb61abde5d32c03d8b91d54f328dd3d6ce5
EPaguia <144369394+EPaguia@users.noreply.github.com>
2024-01-31 21:24:55 +0800

    added html content with javascript

--- script.js
+++ script.js
@@ -10,25 +10,71 @@ const scissors = document.querySelector('.scissors');
 const result = document.querySelector('.result');
 const player = document.querySelector('.player');
 const computer = document.querySelector('.computer');
+const round = document.querySelector('.round');
+let win = 'You win!';
+let lose = 'You lose!';
+let tie = 'It\'s a tie';
+let roundCounter = 1;
+let playerScore = 0;
+let computerScore = 0;
 
 
 
-function playround() {
-    let playerSelection = prompt('Rock, Paper, or Scissors');
-    let computerSelection = getComputerChoice();
-    let win = 'You win!';
-    let lose = 'You lose!';
-    let tie = 'It\'s a tie';
-    playerSelection = playerSelection.toLowerCase();
+function playRound(playerSelection, computerSelection = getComputerChoice()) {
     if ((playerSelection == 'rock' && computerSelection == 'rock') || (playerSelection == 'paper' && computerSelection == 'paper') || (playerSelection == 'scissors' && computerSelection == 'scissors'))
-        {return tie}
+        {return [tie, computerSelection]}
     else if ((playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'rock')){
-        return lose}
+        return [lose, computerSelection]}
     else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')){
-        return win}
-    
+        return [win, computerSelection]}
 }
 
 
+function game(playerChoice){
+    player.textContent = playerChoice;
+    let roundResult = playRound(playerChoice);
+    computer.textContent = roundResult[1];
+    round.textContent = "Round " +roundCounter;
+    if (roundResult[0] == win) {    
+        roundCounter++;
+        playerScore++;
+        result.textContent = win;
+        }
+    else if (roundResult[0] == lose) {
+        roundCounter++;
+        computerScore++;
+        result.textContent = lose;
+        }
+    else if (roundResult[0] = tie) {
+        result.textContent = tie;
+    };
+    if ((playerScore == 3) || (computerScore == 3)) {
+        if (playerScore == 3) {
+            round.textContent = "You win!\nRounds won: "+ playerScore +"\n" + "Rounds lost: " + computerScore;
+            roundCounter = 1;
+            playerScore = 0;
+            computerScore = 0;
+        }
+        else if (computerScore == 3) {
+            round.textContent = "You lose!\nRounds won: "+ playerScore +"\nRounds lost: " + computerScore;
+            roundCounter = 1;
+            playerScore = 0;
+            computerScore = 0;
+        }
 
+    } 
+    }
+
+
+rock.addEventListener('click', () => {
+    game('rock')
+});
+
+paper.addEventListener('click', () => {
+    game('paper');
+    })
+
+scissors.addEventListener('click', () => {
+    game('scissors');
+    })
 
